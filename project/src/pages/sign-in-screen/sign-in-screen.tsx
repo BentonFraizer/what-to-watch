592496da99c8e679a056f5595608c0c51a973c05
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-08-11 16:00:39 +0300

    Доработан экран sign-in-screen - Данные почты и пароля считываются из полей input и во время действия submit отправляются на сервер.

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,19 +1,62 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 
+import { useRef, FormEvent } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types';
+
 function SignInScreen(): JSX.Element {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if(emailRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        eMail: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <div className="user-page">
       <Header isInSignIn/>
       <div className='sign-in user-page__content'>
-        <form action='#' className='sign-in__form'>
+        <form
+          action=''
+          className='sign-in__form'
+          onSubmit={handleSubmit}
+        >
           <div className='sign-in__fields'>
             <div className='sign-in__field'>
-              <input className='sign-in__input' type='email' placeholder='Email address' name='user-email' id='user-email' />
+              <input
+                ref={emailRef}
+                className='sign-in__input'
+                type='email'
+                placeholder='Email address'
+                name='user-email'
+                id='user-email'
+              />
               <label className='sign-in__label visually-hidden' htmlFor='user-email'>Email address</label>
             </div>
             <div className='sign-in__field'>
-              <input className='sign-in__input' type='password' placeholder='Password' name='user-password' id='user-password' />
+              <input
+                ref={passwordRef}
+                className='sign-in__input'
+                type='password'
+                placeholder='Password'
+                name='user-password'
+                id='user-password'
+              />
               <label className='sign-in__label visually-hidden' htmlFor='user-password'>Password</label>
             </div>
           </div>
