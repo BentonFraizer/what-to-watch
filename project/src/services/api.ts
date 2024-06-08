5ad2c9848d411749f8c5792d614fdcbee3e35b33
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-06-08 21:19:28 +0300

    fix(api): change backend url. Restore app functionality

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -13,7 +13,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 
 const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
-const BACKEND_URL = 'https://10.react.pages.academy/wtw';
+const BACKEND_URL = 'https://10.react.htmlacademy.pro/wtw';
 const REQUEST_TIMEOUT = 5000;
 const NOT_FOUND = 404;
 
